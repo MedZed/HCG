eae59a19ba6d66e448ab573238d3ae6afead116d
Med <medy484@gmail.com>
2021-07-15 08:44:49 +0300

    revert htaccess issue

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -20,9 +20,9 @@ class FileUploadController extends Controller
             $save->name = "Screen record";
             $save->path = $file;
             if($save->save()){
-            return back()->with('success','File Has been uploaded successfully');
+            return redirect('/')->with('success','File Has been uploaded successfully');
             }else{
-                return back()->with('fail','There was somthing wrong');
+                return redirect('/')->with('fail','There was somthing wrong');
             }
 
          }
